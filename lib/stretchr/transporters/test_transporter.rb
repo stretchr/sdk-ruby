9bf804179b650163a3f4ac2f956db210387e5962
Mat Ryer <matryer@mac.com>
2013-02-22 12:49:56 -0700

    build test transporter

--- lib/stretchr/transporters/test_transporter.rb
+++ lib/stretchr/transporters/test_transporter.rb
@@ -1,6 +1,20 @@
 class Stretchr::TestTransporter
+
+  attr_accessor :requests, :responses
+
+  def initialize
+    self.requests = []
+    self.responses = []
+  end
+
 	def make_request(request)
 		# store the request and return the next response in the local queue
 		# not NOT actually make any http requests
+
+    self.requests << request
+
+    # return the response
+    self.responses.shift
+
 	end
 end
