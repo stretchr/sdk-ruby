db5e0eb73fd3f3d7674c95049051a758c9ef2dc0
Mat Ryer <matryer@mac.com>
2013-02-22 12:50:49 -0700

    added resource test (unfinished)

---
+++ test/test_resources.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+require 'test_helper.rb'
+require 'stretchr'
+
+class ResourcesTest < Test::Unit::TestCase
+
+  def setup
+    @stretchr = test_stretchr_object
+  end
+
+  def test_new_resource
+
+  end
+
+end
