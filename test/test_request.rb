4425dbdcede89de42c9a5e18465167aab672f5f8
Ryan <rquinn.zk@gmail.com>
2013-10-17 17:50:05 -0600

    tests for url building

--- test/test_request.rb
+++ test/test_request.rb
@@ -11,4 +11,9 @@ describe "Request Object" do
 		r = Stretchr::Request.new({base_url: "https://project.stretchr.com/api/v1.1/"})
 		assert_equal "https://project.stretchr.com/api/v1.1/", r.base_url, "Should let me specify the base url"
 	end
+
+	it "Should know how to build a complete url including path" do
+		r = Stretchr::Request.new({base_url: "https://project.stretchr.com/api/v1.1/"})
+		assert_equal "https://project.stretchr.com/api/v1.1/people/1/cars", r.people(1).cars.to_url, "Should have built the url properly"
+	end
 end
