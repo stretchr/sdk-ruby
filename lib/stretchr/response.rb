e21bc62f353ddf900e35cb9b5331818400372e25
Ryan <rquinn.zk@gmail.com>
2013-10-17 23:25:45 -0600

    store the raw response

--- lib/stretchr/response.rb
+++ lib/stretchr/response.rb
@@ -1,5 +1,8 @@
 module Stretchr
 	class Response
-		
+		attr_reader :raw
+		def initialize(response)
+			@raw = response
+		end
 	end
 end
--- test/test_response.rb
+++ test/test_response.rb
@@ -1,5 +1,8 @@
 require "test_helper"
 
 describe "Response" do
-	it "Should take in and store a response"
+	it "Should take in and store a response" do
+		r = Stretchr::Response.new("test")
+		assert_equal "test", r.raw, "Should have saved the raw response"
+	end
 end
