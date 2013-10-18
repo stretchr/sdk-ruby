0e93efc0d3ba11981a0079d350cef9c5b48dbc56
Ryan <rquinn.zk@gmail.com>
2013-10-17 21:43:40 -0600

    very basic bag functionality

--- test/test_bag.rb
+++ test/test_bag.rb
@@ -6,4 +6,11 @@ describe "Param Bag" do
 		b.set("param", "value")
 		assert_equal "value", b.get("param")
 	end
+
+	it "Should let you merge parameters into a query string" do
+		b = Stretchr::Bag.new
+		b.set("include", "~parent")
+		b.set("key", "asdf")
+		assert_equal "include=~parent&key=asdf", b.query_string, "Should let you create a query string"
+	end
 end
