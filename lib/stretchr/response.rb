54e7a267d352f35fd70b12beeec5fef9c6c933f2
Ryan <rquinn.zk@gmail.com>
2013-10-18 10:06:06 -0600

    requests now parsed using config file values

--- lib/stretchr/response.rb
+++ lib/stretchr/response.rb
@@ -2,14 +2,15 @@ require "json"
 module Stretchr
 	class Response
 		attr_reader :raw, :parsed, :data
-		def initialize(response)
+		def initialize(response, options = {})
+			@api_version = options[:api_version] || Stretchr.config["api_version"]
 			@raw = response
 			@parsed = JSON.parse(response)
-			@data = @parsed["~data"]
+			@data = @parsed[Stretchr.config[@api_version]["data"]]
 		end
 
 		def success?
-			@parsed["~status"] >= 200 && @parsed["~status"] <= 299
+			@parsed[Stretchr.config[@api_version]["status"]] >= 200 && @parsed[Stretchr.config[@api_version]["status"]] <= 299
 		end
 	end
 end
