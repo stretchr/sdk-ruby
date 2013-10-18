767551bc94bc9504103779853f29601c69d92191
Ryan <rquinn.zk@gmail.com>
2013-10-18 09:44:42 -0600

    added test for parsing out data from responses

--- test/test_response.rb
+++ test/test_response.rb
@@ -11,4 +11,10 @@ describe "Response" do
 		r = Stretchr::Response.new(load_api_response("get_collection_response.json"))
 		assert r.success?, "Should have registered response as success"
 	end
+
+	it "Should parse out the json data" do
+		d = {name: "ryon"}.to_json
+		r = Stretchr::Response.new(d)
+		assert_equal "ryon", r.parsed["name"], "Should have parsed out the data"
+	end
 end
